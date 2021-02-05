bf34669d0dc267cf30eafceee253d917e9ee6497
zahrakhadijha <zahra.khan1@icloud.com>
2021-02-05 15:55:40 -0500

    add a count state

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
 
-
 class App extends Component {
-  render() {
-    return <>
-      <h1>Counter</h1>
-      <button>+</button>
-      <span>0</span>
-      <button>-</button>
-      </>
+  constructor() {
+    super()
+
+    this.state = {
+      count: 0,
+    }
   }
 
+  render() {
+    return (
+      <>
+        <h1>Counter</h1>
+        <button>+</button>
+        <span>{this.state.count}</span>
+        <button>-</button>
+      </>
+    )
+  }
 }
 
 export default App
